334c4c8c5ecb85ea1d597471e5050672eb3c4903
alexander-w-o-s <53860987+alexander-w-o-s@users.noreply.github.com>
2020-03-06 17:53:20 +0200

    Cleanup

--- index-cli.js
+++ index-cli.js
@@ -2,13 +2,7 @@
 
 const {run: runAndroid} = require('./src/android');
 const {run: runiOS} = require('./src/ios');
-const getApplicationInfo = require('./src/libs/react-native-app-info');
 
-const info = getApplicationInfo();
-if (!info) {
-  return;
-}
-
-const {basePath} = info;
+const basePath = process.cwd();
 runAndroid(basePath);
 runiOS(basePath);
