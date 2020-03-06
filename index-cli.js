566e7e4be2be55ef58eaba34673657c08a5f48c2
alexander-w-o-s <53860987+alexander-w-o-s@users.noreply.github.com>
2020-03-06 18:12:17 +0200

    CLI - make possible to specify relative path as argument

--- index-cli.js
+++ index-cli.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const {run: runAndroid} = require('./src/android');
 const {run: runiOS} = require('./src/ios');
 
+// Get Current Working Directory as base path or use first argument as path
 const [argBasePath] = process.argv.slice(2);
-const basePath = argBasePath || process.cwd();
+const basePath = !argBasePath ? process.cwd() : path.resolve(process.cwd(), argBasePath);
 
+// Run for all platforms
 runAndroid(basePath);
 runiOS(basePath);
